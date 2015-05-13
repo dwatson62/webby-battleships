6c68253ebac89675128c9d29c38536067a488e6c
Daryl Watson <darylwtsn@gmail.com>
2015-05-13 13:49:31 +0100

    Two players can place a ship

--- features/placing_a_ship.feature
+++ features/placing_a_ship.feature
@@ -5,5 +5,15 @@ Feature: Placing a ship
 
   Scenario: Placing
     Given I am on the place ship page
-    When I enter "B3"
+    When I fill in "form" with "B3" within "place ship"
     Then I should see a battleship on "B3"
+
+  Scenario: Placing
+    Given I am on the place ship page
+    When I fill in "form" with "A3" within "place ship"
+    Then I should see a battleship on A5
+
+  Scenario: Placing
+    Given I am on the place ship page
+    When I fill in "form" with "B3" and "vertical" within "place ship"
+    Then I should see a battleship on "A7"
--- lib/battleships.rb
+++ lib/battleships.rb
@@ -14,15 +14,45 @@ class McBattleships < Sinatra::Base
     erb :enter_name
   end
 
+  get '/name2' do
+    erb :enter_name2
+  end
+
   get '/game' do
+    $name1 = params[:name1]
     $game = Game.new Player, Board
-    erb :place_ship
+    erb :place_ship1
   end
 
   post '/game' do
     $coords = params[:coords]
-    $game.player_1.place_ship Ship.battleship, $coords
-    erb :view_board
+    $direction = params[:direction]
+    if $direction == 'vertical'
+      $game.player_1.place_ship Ship.battleship, $coords, $direction
+    else
+      $game.player_1.place_ship Ship.battleship, $coords
+    end
+    erb :view_board1
+  end
+
+  get '/game2' do
+    $name2 = params[:name2]
+    erb :place_ship2
+  end
+
+  post '/game2' do
+    $coords = params[:coords]
+    $direction = params[:direction]
+    if $direction == 'vertical'
+      $game.player_2.place_ship Ship.battleship, $coords, $direction
+    else
+      $game.player_2.place_ship Ship.battleship, $coords
+    end
+    erb :view_board2
+  end
+
+  get '/finish' do
+    erb :finish
   end
 
   # start the server if ruby file executed directly
